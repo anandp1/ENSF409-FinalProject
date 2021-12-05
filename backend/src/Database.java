41d53bf673b0aee24fea1383f552b267464f56da
Anand Patel <anandpp2001@gmail.com>
2021-12-04 22:50:16 -0700

    Update Database.java
    added more functions needed

--- backend/src/Database.java
+++ backend/src/Database.java
@@ -30,9 +30,14 @@ public class Database {
         }
     }
     // function to get all the properties available
+    // function that returns all renters
+    // function that returns all managers
     // (apartmentType, numBath, numBed, cityQuad, furnishedState) <-- function that returns all properties that match this function returns a arraylist
-    // (email,password) <-- function that returns a string saying if it is a landlord, manager, or registered renter
+    // (email,password) <-- function that returns which landlord, manager or registered renter it is as a string (identified as a id or name or whatever we chose to do)
                             // returns empty string if none of the above
+    // (email, password, IDofperson) <-- function that returns properties owned by landlord if the id is of a landlord
+                                        // returns any mail that landlord has if the id is of a landlord
+                                        
     // (firstName, lastName, email, password) <-- function that saves all this info into the database for the renter
     // (message, emailAddressOfRenter) <-- saves message in landlord email in the database of that renter
 }
