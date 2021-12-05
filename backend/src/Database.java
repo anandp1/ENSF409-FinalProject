ad06edafb2155177028325229aee7b84bd28cade
Anand Patel <anandpp2001@gmail.com>
2021-12-04 20:02:31 -0700

    database class & mainapplication.java

--- backend/src/Database.java
+++ backend/src/Database.java
@@ -1,4 +1,33 @@
-public class Database {
+import java.sql.*;
+import java.util.*;
 
+public class Database {
+    public final String DBURL;
+    public final String USERNAME;
+    public final String PASSWORD;
+
+    private Connection dbConnect;
+    private ResultSet results;
+    public Database(String DBURL, String USERNAME, String PASSWORD) {
+        this.DBURL = DBURL;
+        this.USERNAME = USERNAME;
+        this.PASSWORD = PASSWORD;
+
+    }
+    public void connect() throws SQLException {
+        DriverManager.setLoginTimeout(5);
+        dbConnect = DriverManager.getConnection(DBURL, USERNAME, PASSWORD);
+
+    }
+    public void disconnect() {
+        try {
+            dbConnect.close();
+
+        }
+        catch (Exception e) {
+            System.err.println("Could not connect to Database!");
+            e.printStackTrace();
+        }
+    }
 
 }
---
+++ backend/src/MainApplication.java
@@ -0,0 +1,8 @@
+public class MainApplication {
+    public static void main(String[] args) throws Exception {
+        // connect database we will run our entire code through this
+        // Database test = new Database();
+        //test.connect();
+
+    }
+}
