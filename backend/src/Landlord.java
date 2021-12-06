ad844dcb32abfd203860ef37c338d782b8f29b7e
Anand Patel <anandpp2001@gmail.com>
2021-12-05 18:04:14 -0700

    RegisteredRenter.java
    notification comments

--- backend/src/Landlord.java
+++ backend/src/Landlord.java
@@ -12,6 +12,7 @@ public class Landlord {
         // add it to all the properties in the database
         // in the database each property has a id to it indicating the landlord that in charge of it
 
+
     }
     public void messageRenter(String message, int renterID) {
         // save message for the renterID in the database
@@ -22,6 +23,11 @@ public class Landlord {
     public void postProperty() {
         // turns the property that is associated with this landlords id
         // into active only if it its currently suspended
+        /*
+        One option to notify registeredrenters make a new column in the database
+        called isNew, whenever we add a new property we mark it as a 1 and all the original
+        properties is a 0. See RegisteredRenters class for more.
+         */
     }
     public ArrayList<Property> getProperty() {
         return properties;
--- backend/src/RegisteredRenter.java
+++ backend/src/RegisteredRenter.java
@@ -21,7 +21,15 @@ public class RegisteredRenter {
         // adds criteria to the database
         // adds the renterID to the criteria in the database
     }
-
+    public ArrayList<Criteria> checkSubCriteria() {
+        // ONLY if subscription state is set to true
+        /*
+        We check the criteria against every single property if any of them matches
+        AND it is marked as a 1 in the 'isNew' column then we add it to the arraylist and return
+        it.
+         */
+        return new ArrayList<Criteria>();
+    }
     public void removeCriteria() {
         // unsubscribes that criteria
         // removes the renterID from that criteria
