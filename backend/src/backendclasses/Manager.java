57c5567bf5c4dff22d43d5f9bfd62eac9fbc1355
Anand Patel <anandpp2001@gmail.com>
2021-12-09 02:17:42 -0700

    Manager.java -- backend

--- backend/src/backendclasses/Manager.java
+++ backend/src/backendclasses/Manager.java
@@ -3,6 +3,7 @@ package backendclasses;
 import database.Database;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Manager {
     private ArrayList<Property> allProperties;
@@ -12,15 +13,16 @@ public class Manager {
     }
     public ArrayList<Property> getProperties() {
         // calls database to return all properties
-        return new ArrayList<Property>();
+
+        return db.getAllActiveProperties();
     }
     public ArrayList<String> getRenters() {
         // calls database to return all renters name
-        return new ArrayList<String>();
+        return db.getAllRenterNames();
     }
     public ArrayList<String> getLandlords() {
         // calls database to return all renters name
-        return new ArrayList<String>();
+        return db.getAllLandlordNames();
     }
     public void updateFee(int propertyID, double fee) {
         // get property that matches this name
@@ -33,30 +35,63 @@ public class Manager {
     public void setSummaryInfo(int period) {
         // call database to get all properties that match this period
         allProperties = new ArrayList<Property>();
+        for(Property property : getProperties()) {
+            if(property.getListing().getFee().getPeriod() == period) {
+                allProperties.add(property);
+            }
+        }
     }
     public int getNumListed() {
         // use allProperties to get number of properties in the period
-        return 1;
+        return allProperties.size();
     }
     public int getNumRented() {
         // use allProperties to get number of properties rented in that period
-        return 1;
+        int numRented = 0;
+        for(Property property : allProperties) {
+            if(property.getListing().getListingState().getInt() == 2) {
+                numRented++;
+            }
+        }
+        return numRented;
     }
     public int getNumActive() {
         // use allProperties to get number of properties listed as rented
-        return 1;
+        int numActive = 0;
+        for(Property property : allProperties) {
+            if(property.getListing().getListingState().getInt() == 1) {
+                numActive++;
+            }
+        }
+        return numActive;
     }
     public ArrayList<ArrayList<String>> listNumRented() {
+        // TO DO:
+        // Add landlords name to the array
+
+        ArrayList<ArrayList<String>> listNumRented = new ArrayList<>();
+        int i = 0;
+        for(Property property : allProperties) {
+            if(property.getListing().getListingState().getInt() == 2) {
+                listNumRented.add(new ArrayList<>());
+                listNumRented.get(i).add(
+                        "Property ID: " + String.valueOf(property.getPropertyID()) +
+                        " Address: " + property.getPropertyAddress()
+                );
+                i++;
+            }
+        }
         // ONLY RENTED
         // make a new arraylist with only properties that are rented
         // call database to return landlord name that matches properties of the arraylist
             // basically pass the arraylist as a argument
         // uses allProperties to get property id number and append it to the arraylist
         // uses allProperties to get property address and append it to the arraylist
-        return new ArrayList<ArrayList<String>>();
+        return listNumRented;
     }
-    public void setListingState(String propertyName, String propertyState) {
+    public void setListingState(int propertyID, String propertyState) {
         // get property that matches this name
         // update the property with the name with the new state in the database
+        db.setListingState(Objects.requireNonNull(State.fromInt(State.fromString(propertyState))), propertyID);
     }
 }
--- backend/src/backendclasses/State.java
+++ backend/src/backendclasses/State.java
@@ -12,4 +12,33 @@ public enum State {
 	State(int i) {
 		number = i;
 	}
+
+
+	static public State fromInt(int i) {
+		switch(i) {
+			case 1:
+				return ACTIVE;
+			case 2:
+				return RENTED;
+			case 3:
+				return CANCELLED;
+			case 4:
+				return SUSPENDED;
+		}
+		return null;
+	}
+
+	static public int fromString(String i) {
+		switch(i) {
+			case "ACTIVE":
+				return ACTIVE.getInt();
+			case "RENTED":
+				return RENTED.getInt();
+			case "CANCELLED":
+				return CANCELLED.getInt();
+			case "SUSPENDED":
+				return SUSPENDED.getInt();
+		}
+		return -1;
+	}
 }
